583f64a27edb8f7a6d47a2fe1ad11d5487019f9c
Aditya Pachupate <Aditya.Pachupate@cognizant.com>
2025-04-04 14:31:51 +0530

    Add EcommerceWebApi project with dependencies and config
    This commit introduces the `EcommerceWebApi` project to the solution, including its configuration for Debug and Release modes. Key changes include:
    
    - Updated assembly versioning in `AssemblyInfo.cs`.
    - New interfaces `ICartOperations` and `IOrderRepo` added.
    - Enhanced logging configurations in `appsettings.json`.
    - Created `EcommerceWebApi.csproj` with necessary SDK and package references, including `Swashbuckle.AspNetCore` for API documentation.
    - Added global using directives for improved code organization.
    - Introduced launch settings for different profiles (HTTP, HTTPS, IIS Express).
    - Updated various cache and project files to reflect the new structure and dependencies.

---
+++ Ecomm-Database-Class/Ecomm-Database-Class/Repository/IRepository/IOrderRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecomm_Database_Class.Repository.IRepository
+{
+    public interface IOrderRepo
+    {
+    }
+}
