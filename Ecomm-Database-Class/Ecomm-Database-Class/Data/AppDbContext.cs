b173ba371d9afd482418eec20a723f211ddc53fb
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-07 17:47:41 +0530

    Update AppDbContext.cs

--- Ecomm-Database-Class/Ecomm-Database-Class/Data/AppDbContext.cs
+++ Ecomm-Database-Class/Ecomm-Database-Class/Data/AppDbContext.cs
@@ -27,6 +27,72 @@ namespace Ecomm_Database_Class.Data
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=ECommerce;Integrated Security=True;TrustServerCertificate=true");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<Product>()
+        .HasOne(c => c.Category)
+        .WithMany()
+        .HasForeignKey(sc => sc.CategoryId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    modelBuilder.Entity<Product>()
+        .HasOne(c => c.SubCategory)
+        .WithMany()
+        .HasForeignKey(sc => sc.SubCategoryId)
+        .OnDelete(DeleteBehavior.NoAction);
+
+    //modelBuilder.Entity<Category>()
+    //    .HasMany(c => c.Products)
+    //    .WithOne(p => p.Category!)
+    //    .HasForeignKey(p => p.CategoryId);
+
+    //modelBuilder.Entity<SubCategory>()
+    //    .HasMany(sc => sc.Products)
+    //    .WithOne(p => p.SubCategory!)
+    //    .HasForeignKey(p => p.SubCategoryId);
+
+    // Seed Category
+    modelBuilder.Entity<Category>().HasData(
+        new Category { Id = 1, Name = "Luxury" },
+        new Category { Id = 2, Name = "Casual" }
+    );
+
+    // Seed SubCategory
+    modelBuilder.Entity<SubCategory>().HasData(
+        new SubCategory { Id = 1, Name = "Analog", CategoryId = 1 },
+        new SubCategory { Id = 2, Name = "Digital", CategoryId = 2 }
+    );
+
+    // Seed Product
+    modelBuilder.Entity<Product>().HasData(
+        new Product
+        {
+            Id = 1,
+            Name = "Omega Seamaster",
+            Description = "Luxury dive watch",
+            Price = 2999.99M,
+            Brand = "Omega",
+            SoldCount = 120,
+            CategoryId = 1,
+            SubCategoryId = 1
+        },
+        new Product
+        {
+            Id = 2,
+            Name = "Casio G-Shock",
+            Description = "Rugged digital watch",
+            Price = 149.99M,
+            Brand = "Casio",
+            SoldCount = 500,
+            CategoryId = 2,
+            SubCategoryId = 2
+        }
+        );
+
+}
+
 
         #region DB1
         public class DB1
